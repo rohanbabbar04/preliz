f8f23786c70004cdf01e281b2f70e60cee09eabb
Rohan Babbar <rohanbabbar0408@yahoo.com>
2026-08-13 00:30:06 +0530

    Fix linting

--- preliz/distributions/wishart.py
+++ preliz/distributions/wishart.py
@@ -125,18 +125,19 @@ class Wishart(Continuous):
         )
 
 
+# Excluding as slogdet_specialization does not support ndim > 2
 @pytensor_jit(static_shapes={'V': (-1, )},
-              mode=get_mode("NUMBA").excluding("slogdet_specialization")) # Excluding as it doesn't support ndim > 2.
+              mode=get_mode("NUMBA").excluding("slogdet_specialization"))
 def ptd_pdf(x, nu, V):
     return ptd_wishart.pdf(x, nu, V)
 
 @pytensor_jit(static_shapes={'V': (-1, )},
-              mode=get_mode("NUMBA").excluding("slogdet_specialization")) # Excluding as it doesn't support ndim > 2.
+              mode=get_mode("NUMBA").excluding("slogdet_specialization"))
 def ptd_logpdf(x, nu, V):
     return ptd_wishart.logpdf(x, nu, V)
 
 @pytensor_jit(static_shapes={'V': (-1, )},
-              mode=get_mode("NUMBA").excluding("slogdet_specialization")) # Excluding as it doesn't support ndim > 2.
+              mode=get_mode("NUMBA").excluding("slogdet_specialization"))
 def ptd_entropy(nu, V):
     return ptd_wishart.entropy(nu, V)
 
